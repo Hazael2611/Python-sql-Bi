e1df0b0af8680f89fe0e240960dd64a1c7c646b5
Hazael Hinojosa Hernandez <hazael.hhdez@hotmail.com>
2025-09-01 10:28:22 -0600

    Update SQL_Queries.sql
    4 y 5 query

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -15,6 +15,8 @@ SELECT
 FROM
 	general_info
 
+
+	
 --Second Query to get top 10 State with more churn rate and general info
 
 WITH churn_by_state AS (
@@ -35,6 +37,9 @@ FROM (
     FROM churn_by_state
 ) ranked
 WHERE churn_rank <= 10
+
+
+	
 --Third Query: to get avg behaivor group by churn
 SELECT
 AVG("Total day minutes") as Total_day_min,
@@ -45,3 +50,34 @@ AVG("Customer service calls") as Customer_service_calls
 FROM
 churn
 GROUP BY "Churn"
+
+	
+--Fourth Query: to identify if exist a linear relation between more customer calls and churn
+WITH service_calls AS (
+    SELECT 
+        "Customer service calls",
+        "Churn",
+        COUNT(*) AS customers
+    FROM churn
+    GROUP BY "Customer service calls", "Churn"
+)
+SELECT *
+FROM service_calls
+ORDER BY "Customer service calls" DESC, "Churn" DESC;
+
+
+-- Fifth quey, to find and indentify if exist a relation between the international plan and churn cases
+WITH International_comparative AS (
+    SELECT 
+        "International plan",
+        "Churn",
+        COUNT(*) AS customers
+    FROM churn
+    GROUP BY "International plan", "Churn"
+)
+SELECT 
+*,
+-- calculate the rate or percent, sum over partition is used to sum customers with the same international plan no metters of the churn column
+ROUND(customers/SUM(customers) OVER (PARTITION BY "International plan"),4) as percent_plans
+FROM International_comparative
+ORDER BY "International plan" DESC, "Churn" DESC;
